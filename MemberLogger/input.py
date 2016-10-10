299e31bebd531321cdf753c63bdd17d20e21474e
Your Name <tinus.scheppers@telenet.be>
2016-10-10 21:35:13 +0200

    Works on Pi, more OOP and event scanning.

--- MemberLogger/input.py
+++ MemberLogger/input.py
@@ -1,25 +1,33 @@
+import pyxhook
+import database
+
 class input(object):
 
 	memberIDList = list()
 	prevEvent = str()
 	numbers = {"agrave":0,"ampersand":1,"eacute":2,"quotedbl":3,"apostrophe":4,"parenleft":5,"section":6,"egrave":7,"exclam":8,"ccedilla":9}
+	db = database.database()
+
+	def keyboardEvent(self,event):
+		wasThereAnEvent = False
+
+		# if event key is number
+		if event.Key in self.numbers and (self.prevEvent in self.numbers or not self.prevEvent):
+			self.memberIDList.append(self.numbers[event.Key])
+		elif event.Key == "Return" and len(self.memberIDList) == 10:
+			print(str(self.memberIDList))
+			# write scan to db
+			self.db.writePresence(''.join(str(idInt) for idInt in self.memberIDList))
+			self.memberIDList.clear()
+		else:
+			print(str(event.Key))
 
 	def __init__(self):
 		#Create hookmanager
-		hookman = pyxhook.HookManager()
+		self.hookman = pyxhook.HookManager()
 		#Define our callback to fire when a key is pressed down
-		hookman.KeyDown = keyboardEvent
+		self.hookman.KeyDown = self.keyboardEvent
 		#Hook the keyboard
-		hookman.HookKeyboard()
+		self.hookman.HookKeyboard()
 		#Start our listener
-		hookman.start()
-
-	def keyboardEvent(event):
-		wasThereAnEvent = False
-	
-		# if event key is number
-		if event.Key in numbers and (prevEvent in numbers or not prevEvent):
-			memberIDList.append(numbers[event.key])
-		elif event.Key == "Return":
-			print("test");
-		return True
+		self.hookman.start()
---
+++ MemberLogger/main.py
@@ -0,0 +1,13 @@
+import input
+from time import sleep
+
+def main():
+	scanner = input.input()
+	while True:
+		sleep(1)
+
+if __name__ == "__main__": 
+	main()
+
+
+
